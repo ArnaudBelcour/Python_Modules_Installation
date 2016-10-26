35dd98188967436efe5437a2061f8722079ab097
Arnaud Belcour <arnaud.belcour@gmail.com>
2016-10-26 11:47:29 +0200

    Rewrite text_processing.py

--- text_processing.py
+++ text_processing.py
@@ -2,31 +2,29 @@
 from moduleInstallation import ModuleInstallation
 import sys
 
-moduleInstall = ModuleInstallation("nltk", ['punkt', 'averaged_perceptron_tagger'])
+def nltkCheckAndInstallation():
+	moduleInstall = ModuleInstallation("nltk", ['punkt', 'averaged_perceptron_tagger'])
 
-#Check and, if not present, instal nltk and needed packages
-choiceSentence =  "Do you want to check " + moduleInstall.module + " and his packages to see if they are up-to-date for our script : \n\t" + moduleInstall.module +" version " + moduleInstall.moduleVersionUsed
+	#Check and, if not present, instal nltk and needed packages
+	choiceSentence =  "Do you want to check " + moduleInstall.module + " and his packages to see if they are up-to-date for our script : \n\t" + moduleInstall.module +" version " + moduleInstall.moduleVersionUsed
 
-for package in moduleInstall.packages : 
-	choiceSentence += "\n\tpackage " + package 
-choiceSentence += "\nProceed(y/n)?"
+	for package in moduleInstall.packages :
+		choiceSentence += "\n\tpackage " + package
+	choiceSentence += "\nProceed(y/n)?"
 
-if moduleInstall.pythonVersion < (3,0,0): 
-	choice = raw_input(choiceSentence).lower()
-elif moduleInstall.pythonVersion > (3,0,0):
-	choice = input(choiceSentence).lower()
+	if moduleInstall.pythonVersion < (3,0,0):
+		choice = raw_input(choiceSentence).lower()
+	elif moduleInstall.pythonVersion > (3,0,0):
+		choice = input(choiceSentence).lower()
 
-if choice  in moduleInstall.answersYN[0] :
-	moduleInstall.checkModuleInstallation()
-if choice in moduleInstall.answersYN[1] :
-			pass
-
-import nltk
-from nltk import word_tokenize
-from nltk import pos_tag
-from xml.dom import minidom
+	if choice  in moduleInstall.answersYN[0] :
+		moduleInstall.checkModuleInstallation()
+	if choice in moduleInstall.answersYN[1] :
+				pass
 
 def xmlAbstractExtraction(fileName):
+	from xml.dom import minidom
+
 	print ("Parsing and extracting abstracts from corpus")
 	xmldoc = minidom.parse(fileName)
 	l_abstracts = xmldoc.getElementsByTagName('AbstractText')
@@ -38,11 +36,19 @@ def xmlAbstractExtraction(fileName):
 
 	return l_abstractsExtracted
 
-def tokenizationAndTagging(l_abstract)
+def tokenizationAndTagging(l_abstract):
+	import nltk
+	from nltk import word_tokenize
+	from nltk import pos_tag
+
 	for line in l_abstract:
 		#creates tokens of a string
 		tokens = word_tokenize(line.decode('utf-8'))
-
 		#tags tokens with their PoS
 		taggedTokens = pos_tag(tokens)
 		print(str(taggedTokens))
+
+def main():
+	nltkCheckAndInstallation()
+	l_abstract = xmlAbstractExtraction("abstract_ethology_animal")
+	tokenizationAndTagging(l_abstract)
